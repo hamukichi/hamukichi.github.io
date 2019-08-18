08169a5152adea5294d24b91ff4c26d8104d2aa2
Hamukichi (Nombiri) <hamukichi-dev@outlook.jp>
2019-08-18 17:42:15 +0900

    Customized Sharing

--- character-sorter/custom-sharing.js
+++ character-sorter/custom-sharing.js
@@ -1,2 +1,21 @@
 /*jslint nomen:true */
 /*global $ */
+
+
+function sharePhrase() {
+    var sortedPhrase = $('#sorting_result').text();
+    var shareText = "ソートなぞなぞ\n";
+    shareText += sortedPhrase;
+    shareText += "(powered by ${title} ${link})";
+    var a2a_config = a2a_config || {};
+    a2a_config.templates = a2a_config.templates || {};
+    a2a_config.templates.email = {
+        body: shareText
+    };
+    a2a_config.templates.sms = {
+        body: shareText
+    };
+    a2a_config.templates.twitter = {
+        text: shareText,
+    };
+}
